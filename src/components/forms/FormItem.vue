5c3d804ca990d563241263ba024c6ca52c91540d
Stéphane Raynaud <stephane.raynaud@millevolts.fr>
2019-05-29 11:51:58 +0200

    Ajout d'une propriété "help" sur les formitems

--- src/components/forms/FormItem.vue
+++ src/components/forms/FormItem.vue
@@ -4,8 +4,20 @@
       <slot name="label"></slot>
     </template>
     <slot></slot>
+    <div
+      v-if="$slots.help"
+      class="form-item-help text-secondary text-small text-compact"
+    >
+      <slot name="help"></slot>
+    </div>
   </el-form-item>
 </template>
+
+<style>
+.form-item-help {
+  margin-top: 4px;
+}
+</style>
 
 <script>
 import { FormItem as ElFormItem } from "element-ui";
